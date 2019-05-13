bb5801c4e2a3726b8dfdc7fa25a46c066725cb59
Guiliang <gla68@sfu.ca>
2019-05-13 16:06:02 -0700

    2019-5-13

--- generate_cluster/run_cluster.py
+++ generate_cluster/run_cluster.py
@@ -1,4 +1,7 @@
+import sys
+sys.path.append('/Local-Scratch/PycharmProjects/sport-analytic-markov/')
 from ap_cluster import APCluster
+
 
 if __name__ == "__main__":
     soccer_data_dir = '/cs/oschulte/soccer-data/sequences_append_goal/'
